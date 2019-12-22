12c66a216e9d51ac8ed78a09b4a7e27b70bf69a4
Mollydj <55717647+Mollydj@users.noreply.github.com>
2019-12-21 21:46:03 -0500

    Repository in IIFE pattern, forEach loop adjusted accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,16 @@
 var pokemonRepository = (function () {
 var repository = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass',' Poison']
-  },
-  {
-    name: 'Charmander',
-    height: 1.6,
-    types: 'Grass'
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: 'Water'
-  }
+  {name: 'Bulbasaur', height: 0.7, types: ['Grass',' Poison'] },
+  {name: 'Charmander', height: 1.6, types: 'Grass'},
+  {name: 'Squirtle', height: 0.5, types: 'Water'}
 ];
 
   function add(pokemon) {
     repository.push(pokemon);
+  }
+
+    function values(values) {
+    return repository.keys;
   }
 
   function getAll() {
@@ -31,11 +23,11 @@ var repository = [
   };
 })();
 
-
-
 pokemonRepository.add({ name: 'Pikachu' });
-/*console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]*/
 
 Object.keys(pokemonRepository.getAll()).forEach(function(property) {
-  console.log(pokemonRepository.getAll()[property]);
+  document.write(pokemonRepository.getAll()[property].name + ' ');
+  document.write(pokemonRepository.getAll()[property].height + ' ');
+  document.write(pokemonRepository.getAll()[property].types + ' ');
+   document.write('<br>')
 });
