190ae486dee2f4342d807e8014486f98bd134853
Mollydj <55717647+Mollydj@users.noreply.github.com>
2019-12-30 20:31:54 -0500

    Event Listeners & List/Button Elements added
    + Event listener to log pokemon info to the console
    + List Button Elements added
    + $pokemonList Variable created to reference ul to index.HTML
    + Button appended to the list item
    + List item appended to the unordered list in the index/HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
+
 var pokemonRepository = (function () {
-var repository = [
-  {name: 'Bulbasaur', height: 0.7, types: ['Grass',' Poison'] },
-  {name: 'Charmander', height: 1.6, types: 'Grass'},
-  {name: 'Squirtle', height: 0.5, types: 'Water'}
-];
+  var repository = [
+    {name: 'Bulbasaur', height: 0.7, types: ['Grass',' Poison'] },
+    {name: 'Charmander', height: 1.6, types: 'Grass'},
+    {name: 'Squirtle', height: 0.5, types: 'Water'}
+  ];
+
+//>Button and List Informartion
+  function addListItem(pokemon){
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-name');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem); 
+
+//Event Listener here
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
+  //< End of Button and List Information
+
+
+
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
 
   function add(pokemon) {
     repository.push(pokemon);
@@ -15,16 +38,16 @@ var repository = [
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 
 })();
 
 
+var $pokemonList = document.querySelector('ul');
 
-pokemonRepository.getAll().forEach(function(arrayItem) {
-     Object.keys(arrayItem).forEach(function(key){
-         document.write(arrayItem[key])
-     })
- document.write('<br>')
-})
+//forEach Loop
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
